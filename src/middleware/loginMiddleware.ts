14c742de505f0ff38b85c057fb5359d131d9107e
RajapndiR <rajapndi.r@quadkast.co.in>
2024-05-02 16:49:11 +0530

    Changes

--- src/middleware/loginMiddleware.ts
+++ src/middleware/loginMiddleware.ts
@@ -1,11 +1,12 @@
+import { NextFunction } from "express";
 import { Jwt, Message, Request, Responder, Response, UserModel } from "../helpers/path";
 import jwt from "jsonwebtoken"
 class MiddlewareClass {
-    loginMiddleware = async (req: Request, res: Response, next) => {
+    loginMiddleware = async (req: Request, res: Response, next: NextFunction) => {
         try {
             if (!req.headers["authorization"]) return Responder.sendFailureUnAuthMessage(Message.notToken404, res)
             const token = req.headers["authorization"].split(' ')[1]
-            let { userId, } = await Jwt.verifyToken(token);            
+            let { userId, } = await Jwt.verifyToken(token);
             // if (exp > Date.now()) return Responder.sendFailureForbiddenMessage("token expired", res)
             let user = await UserModel.find({ _id: userId }, { password: 0 });
             if (!user) return Responder.sendFailureUnAuthMessage(Message.unAuth, res)
